dcaf8f3f03885fe28a76c193ac68284ade0cfaa2
Sania <saniaarooj955@gmail.com>
2018-01-13 01:49:58 +0500

    Assignment 4

---
+++ app/src/main/java/com/example/salman/uolfall17serviceassignment/CustomMessageEvent.java
@@ -0,0 +1,17 @@
+package com.example.salman.uolfall17serviceassignment;
+
+/**
+ * Created by Salman on 12/21/2017.
+ */
+
+class CustomMessageEvent {
+    private int limit;
+
+    public int getLimit() {
+        return limit;
+    }
+
+    public void setLimit(int limit) {
+        this.limit = limit;
+    }
+}
